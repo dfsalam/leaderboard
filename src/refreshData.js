39db2ac6b46e3d70af917f24e766545bd5c41ce0
dfsalam <dfsalam@gmail.com>
2023-02-03 10:02:26 -0500

    Change .then for async/await in refresh function

--- src/refreshData.js
+++ src/refreshData.js
@@ -1,16 +1,17 @@
-const loadData = async () => {
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HuDs7U0CQZGKymImVaSp/scores/')
-    .then((response) => response.json())
-    .then((data) => {
-      const container = document.querySelector('.recent__container');
-      let dataToLoad = '';
-      for (let i = 0; i < data.result.length; i += 1) {
-        dataToLoad += `
+const loadData = async () => {  
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HuDs7U0CQZGKymImVaSp/scores/')
+  const data = await response.json();
+
+  const container = document.querySelector('.recent__container');
+  let dataToLoad = '';
+  
+  for (let i = 0; i < data.result.length; i += 1) {
+    dataToLoad += `
           <div class="recent__score">Name:${data.result[i].user} ${data.result[i].score}</div>    
           `;
-      }
-      container.innerHTML = dataToLoad;
-    });
+  }
+  container.innerHTML = dataToLoad;
+
 };
 
 export default loadData;
