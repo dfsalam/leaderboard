1620b64cd71aed98b8928bcb91bef06bd1fed941
dfsalam <dfsalam@gmail.com>
2023-02-02 20:06:58 -0500

    Hit the API - Add submit and refresh event listeners. - Add refreshData function to refresh button. - Add submitData function to submit button.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import loadData from './refreshData';
+import submitData from './submitData';
+
+
+
+const refresh = document.querySelector('.refresh');
+const formCnt = document.querySelector('.inputs');
+
+//Load stored data
+loadData();
+
+refresh.addEventListener('click', () => {
+  loadData();
+})
+
+formCnt.addEventListener('submit', (e) => {
+  e.preventDefault();  
+  submitData();  
+})
+
---
+++ src/submitData.js
@@ -0,0 +1,26 @@
+const submitData = async () => {
+  const name = document.querySelector('.input__user');
+  const score = document.querySelector('.input__score');
+  const data = { 
+    'user': name.value, 
+    'score': score.value 
+  };
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HuDs7U0CQZGKymImVaSp/scores/', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      name.value = "";
+      score.value ="";
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+export default submitData;
+
