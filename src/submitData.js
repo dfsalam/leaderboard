2da88a04519dd849548286ff8ae14972be1cdba0
dfsalam <dfsalam@gmail.com>
2023-02-03 10:13:09 -0500

    Change .then for async/await in submit function

--- src/submitData.js
+++ src/submitData.js
@@ -1,25 +1,24 @@
 const submitData = async () => {
   const name = document.querySelector('.input__user');
   const score = document.querySelector('.input__score');
-  const data = {
+  const dataToPost = {
     user: name.value,
     score: score.value,
   };
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HuDs7U0CQZGKymImVaSp/scores/', {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HuDs7U0CQZGKymImVaSp/scores/', {
     method: 'POST', // or 'PUT'
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(dataToPost),
   })
-    .then((response) => response.json())
-    .then(() => {
-      name.value = '';
-      score.value = '';
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  name.value = '';
+  score.value = '';
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  } 
 };
 
 export default submitData;
